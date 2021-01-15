326cf070fea5a1cd1f65936895e0aefa01ba0778
rahmagamal <55654504+rahmagamal@users.noreply.github.com>
2021-01-15 03:45:59 +0200

    contact responsive

--- js/toggle.js
+++ js/toggle.js
@@ -1,9 +1,12 @@
 $(document).ready(function(){
     $(".nav-link ").click(function(){
       $("#navbarTogglerDemo02").hide();
-      $(".navbar-toggler-icon ").click(function(){
-        $("#navbarTogglerDemo02").show();
-      })
-
+  
     });
+    $(".navbar-toggler-icon").click(function(){
+      $("#navbarTogglerDemo02").toggle();
+    })
+  
   });
+
+  
